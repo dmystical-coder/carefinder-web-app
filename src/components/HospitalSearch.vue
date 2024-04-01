ad721534bb9a51963f1a5d27b2f79bc9c68d0b55
dmystical_coder <dmystical.coder@gmail.com>
2024-04-01 16:29:25 +0100

    :star: feat: add search feature

--- src/components/HospitalSearch.vue
+++ src/components/HospitalSearch.vue
@@ -1,63 +1,193 @@
 <template>
-  <div class="hospital-search">
-    <input
-      type="text"
-      v-model="locationQuery"
-      @input="handleSearch"
-      placeholder="Enter your location..."
-    />
-    <select v-model="selectedCity" @change="handleSearch">
-      <option value="">Select a nearby city</option>
-      <option v-for="city in nearbyCities" :key="city">{{ city }}</option>
-    </select>
-    <ul v-if="searchResults.length">
-      <li v-for="hospital in searchResults" :key="hospital.id">
-        <h3>{{ hospital.name }}</h3>
-        <p>Address: {{ hospital.address }}</p>
-        <p>Phone: {{ hospital.phone }}</p>
-        <p>Email: {{ hospital.email }}</p>
-      </li>
-    </ul>
-    <p v-else>No hospitals found.</p>
+  <div class="container">
+    <header>
+      <form @submit.prevent class="search-box">
+        <div class="search-input">
+          <i class="fa fa-search" aria-hidden="true"></i>
+          <input type="text" placeholder="Enter your Location..." v-model.lazy.trim="locationQuery"
+            @keyup.enter="handleSearch" />
+        </div>
+
+        <button class="search-btn" @click="handleSearch">
+          <span>Search</span>
+          <img src="./search-icon.svg" alt="" />
+        </button>
+
+        <!-- <div class="filters">
+            <div>
+              <span>Filter by Cities:</span>
+              <label class="checkbox-label">
+                <input type="checkbox" value="user" />
+                Lagos
+              </label>
+              <label class="checkbox-label">
+                <input type="checkbox" value="repository" />
+                Ogun
+              </label>
+              <label class="checkbox-label">
+                <input type="checkbox" value="repository" />
+                Oyo
+              </label>
+              <label class="checkbox-label">
+                <input type="checkbox" value="repository" />
+                Niger
+              </label>
+            </div>
+            <div>
+              <span>Filter by Providers:</span>
+              <label class="checkbox-label">
+                <input type="checkbox" value="user" />
+                Clinic
+              </label>
+              <label class="checkbox-label">
+                <input type="checkbox" value="repository" />
+                Hospital
+              </label>
+              <label class="checkbox-label">
+                <input type="checkbox" value="repository" />
+                Gym
+              </label>
+              <label class="checkbox-label">
+                <input type="checkbox" value="repository" />
+                Spa
+              </label>
+            </div>
+            
+            <button>
+              <i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Apply Filters
+            </button>
+          </div> -->
+      </form>
+
+    </header>
+    <main>
+
+
+      <div v-if="allHospitals.length || searchedHospitals.length" class="search-box search-result-text-box">
+        <span v-if="allHospitals.length">Showing {{ allHospitals.length }} results</span>
+        <span v-else-if="searchedHospitals.length">Showing {{ searchedHospitals.length }} results for {{
+            validLocationQuery
+          }}</span>
+
+        <div>
+          <i class="fa fa-file-text fa-2x" aria-hidden="true"></i>
+          <i class="fa fa-share-alt fa-2x" aria-hidden="true"></i>
+        </div>
+      </div>
+
+      <div class="grid-container" v-if="allHospitals.length">
+        <div class="detail-card" v-for="  hospital   in   allHospitals  " :key="hospital.id">
+          <img :src="hospital.img_url" alt="" />
+          <div class="content">
+            <div class="name">
+              <i class="fa fa-hospital" aria-hidden="true"></i>&nbsp;
+              <h3>{{ hospital.name }}</h3>
+            </div>
+            <div class="type">
+              <img src="./hospital.svg" alt="" />
+              &nbsp;
+              <span>Hospital</span>
+            </div>
+            <div class="address">
+              <img src="./location-marker.svg" /> &nbsp;
+              <span>{{ hospital.address }}</span>
+            </div>
+            <div class="phone">
+              <i class="fa fa-phone" aria-hidden="true"></i> &nbsp;
+              <span>{{ hospital.phone_number }}</span>
+            </div>
+            <div class="email">
+              <i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;
+              <span>{{ hospital.email }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid-container" v-else-if="searchedHospitals.length">
+        <div class="detail-card" v-for="  hospital   in   searchedHospitals  " :key="hospital.id">
+          <img :src="hospital.img_url" alt="" />
+          <div class="content">
+            <div class="name">
+              <i class="fa fa-hospital" aria-hidden="true"></i>&nbsp;
+              <h3>{{ hospital.name }}</h3>
+            </div>
+            <div class="type">
+              <img src="./hospital.svg" alt="" />
+              &nbsp;
+              <span>Hospital</span>
+            </div>
+            <div class="address">
+              <img src="./location-marker.svg" /> &nbsp;
+              <span>{{ hospital.address }}</span>
+            </div>
+            <div class="phone">
+              <i class="fa fa-phone" aria-hidden="true"></i> &nbsp;
+              <span>{{ hospital.phone_number }}</span>
+            </div>
+            <div class="email">
+              <i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;
+              <span>{{ hospital.email }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p v-else>No hospitals found.</p>
+    </main>
   </div>
 </template>
 
 <script>
 import { db } from '@/firebase/firebase'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, query, where, or, and } from 'firebase/firestore'
+
+// create a reference to the hospitals collection
+const hospitalsRef = collection(db, "hospitals");
 
 export default {
   name: 'HospitalSearch',
-  data() {
-    return {
-      locationQuery: '',
-      selectedCity: '',
-      searchResults: [],
-      nearbyCities: ['Gbagada', 'Victoria Island', 'Ikeja'] // Replace with actual nearby cities
+  data: () => ({
+    locationQuery: '',
+    allHospitals: [],
+    searchedHospitals: [],
+  }),
+  created() {
+    this.fetchData()
+  },
+  computed: {
+    validLocationQuery() {
+      return this.locationQuery.replace(
+        /\w\S*/g,
+        function (txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+      );
     }
   },
   methods: {
     async handleSearch() {
-      const query = this.locationQuery.trim()
-      const city = this.selectedCity.trim()
-
-      if (!query && !city) {
-        this.searchResults = []
-        return
-      }
-
-      // let hospitalsRef = db.collection('hospitals')
-
-      // if (query) {
-      //   hospitalsRef = hospitalsRef.where('location', '==', query)
-      // } else if (city) {
-      //   hospitalsRef = hospitalsRef.where('city', '==', city)
-      // }
-
+      this.searchedHospitals = []
+      this.allHospitals = [];
       try {
-        const querySnapshot = await getDocs(collection(db, 'hospitals'))
+        // Create a query against the collection.
+        const q = query(hospitalsRef, where('city', '==', this.validLocationQuery)
+        );
+        const querySnapshot = await getDocs(q);
         querySnapshot.forEach((doc) => {
-          console.log(`${doc.id} => ${doc.data()}`)
+          // doc.data() is never undefined for query doc snapshots
+          this.searchedHospitals.push({ ...doc.data(), id: doc.id })
+        })
+      }
+      catch (error) {
+        console.error('Error searching hospitals:', error)
+      };
+
+    },
+    async fetchData() {
+      try {
+        const querySnapshot = await getDocs(hospitalsRef)
+        querySnapshot.forEach((doc) => {
+          this.allHospitals.push({ ...doc.data(), id: doc.id })
+          console.log(this.allHospitals)
         })
       } catch (error) {
         console.error('Error searching hospitals:', error)
@@ -69,4 +199,92 @@ export default {
 
 <style scoped>
 /* Add your styles here */
+.search-box {
+  display: flex;
+  padding: 1.6875rem 1.5rem;
+  flex-direction: column;
+  gap: 1.8125rem;
+  border-radius: 0.9375rem;
+  background: #fff;
+
+  /* shadow 1 */
+  box-shadow: 6px 6px 35px 0px rgba(16, 40, 81, 0.11);
+}
+
+.search-input {
+  color: #5c6169;
+  padding: 0.81rem 0 0.81rem 1rem;
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  border-radius: 0.5rem;
+  border: 1px solid #2aa8ff;
+  background: #fafbfe;
+}
+
+.search-input input {
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 500;
+  background: transparent;
+  border: none;
+  outline: none;
+  padding: 0.25rem;
+  width: 100%;
+}
+
+input::placeholder {
+  color: #abb6c7;
+}
+
+.search-btn {
+  color: #fff;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: 0.02rem;
+  display: flex;
+  padding: 0.75rem 3.25rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  border-radius: 0.5rem;
+  background: #2aa8ff;
+  outline: none;
+  border: none;
+}
+
+.search-result-text-box {
+  border-radius: 0.5rem;
+  color: #fafbfe;
+  background: linear-gradient(92deg, #2aa7ff 1.4%, #0c8ce5 100.57%);
+  margin-block: 1.5rem;
+  padding: 1rem;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.search-result-text-box span {
+  font-size: 1.2rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+}
+
+.search-result-text-box>div {
+  display: flex;
+  gap: 0.75rem;
+}
+
+.search-result-text-box i {
+  color: #ffffff;
+}
+
+@media screen and (min-width: 768px) {
+  .search-result-text-box {
+    margin: 1.5rem 2rem;
+  }
+}
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,291 @@
 <template>
-  <header>
+  <div class="container">
     <nav>
-      <router-link to="/signup">Sign Up</router-link>
-      <router-link to="/signin">Sign In</router-link>
+      <div class="logo">
+        <img src="/logo.svg" alt="" />
+      </div>
+      <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+      <span class="hidden"><i class="fa fa-times fa-2x" aria-hidden="true"></i></span>
+      <ul class="hidden">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#" class="btn">Login</a></li>
+        <li><a href="#" class="btn">Signup</a></li>
+      </ul>
     </nav>
-  </header>
-  <main>
-    <h1>Homepage</h1>
-  </main>
+    <header>
+      <div class="hero">
+        <div class="hero-content">
+          <h1>Find, Export, and Share Hospitals within your Region.</h1>
+        </div>
+
+        <a href="./search.html">Find Hospitals</a>
+      </div>
+    </header>
+    <main>
+      <h2>Features</h2>
+      <div class="grid-container">
+        <div class="detail-card">
+          <div class="name">
+            <i class="fa fa-search fa-2x" aria-hidden="true"></i>
+            &nbsp;
+            <h3>Search</h3>
+          </div>
+          <hr />
+          <div class="type">
+            <p>
+              Carefinder allows users to search for hospitals within their
+              region, and view the list of hospitals and their respective
+              contact details.
+            </p>
+          </div>
+        </div>
+        <div class="detail-card">
+          <div class="name">
+            <i class="fa fa-filter fa-2x" aria-hidden="true"></i>
+            &nbsp;
+            <h3>Filter</h3>
+          </div>
+          <hr />
+          <div class="type">
+            <p>
+              Carefinder lists hospitals based on the user's location or based
+              on the user's selection from a list of nearby cities within the
+              area.
+            </p>
+          </div>
+        </div>
+
+        <div class="detail-card">
+          <div class="name">
+            <i class="fa fa-file-text fa-2x" aria-hidden="true"></i>
+            &nbsp;
+            <h3>Export</h3>
+          </div>
+          <hr />
+          <div class="type">
+            <p>
+              Carefinder allows users to export the list of hospitals to a CSV
+              file, making it easy to save and share the information.
+            </p>
+          </div>
+        </div>
+        <div class="detail-card">
+          <div class="name">
+            <i class="fa fa-share-alt fa-2x" aria-hidden="true"></i>
+            &nbsp;
+            <h3>Share</h3>
+          </div>
+          <hr />
+          <div class="type">
+            <p>
+              Carefinder also allows users to share the list of hospitals with
+              others. Users can share the information via email or by
+              generating a shareable link.
+            </p>
+          </div>
+        </div>
+      </div>
+      <h2>Featured Hospitals</h2>
+      <div class="grid-container">
+        <div class="detail-card">
+          <!-- <div class="image">
+              <img
+                src="https://lh5.googleusercontent.com/p/AF1QipOWSZ0l_Xety_7BDoj0BiYjh8dQ771UeVr2FxXy=w408-h306-k-no"
+                alt=""
+              />
+            </div> -->
+          <img src="https://lh5.googleusercontent.com/p/AF1QipOWSZ0l_Xety_7BDoj0BiYjh8dQ771UeVr2FxXy=w408-h306-k-no"
+            alt="" />
+          <div class="content">
+            <div class="name">
+              <i class="fa fa-hospital" aria-hidden="true"></i>&nbsp;
+              <h3>Havana Specialist Hospital Limited</h3>
+            </div>
+            <div class="type">
+              <img src="./hospital.svg" alt="" />
+              &nbsp;
+              <span>Hospital</span>
+            </div>
+            <div class="address">
+              <img src="./location-marker.svg" /> &nbsp;
+              <span>115 Akerele Extension, 115 Akerele Road, Surulere, Lagos
+                100001</span>
+            </div>
+            <div class="phone">
+              <i class="fa fa-phone" aria-hidden="true"></i> &nbsp;
+              <span>08027272727</span>
+            </div>
+            <div class="email">
+              <i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;
+              <span>info@havanaspecialisthospital.com</span>
+            </div>
+          </div>
+        </div>
+        <div class="detail-card">
+          <img src="https://lh5.googleusercontent.com/p/AF1QipMfy1m5p2XEkjHpfwy24ytmgNWJvo-c5TzxkzQL=w426-h240-k-no"
+            alt="" />
+          <div class="content">
+            <div class="name">
+              <i class="fa fa-hospital" aria-hidden="true"></i>&nbsp;
+              <h3>Lagos State University Primary Health Care Centre</h3>
+            </div>
+            <div class="type">
+              <i class="fa fa-medkit" aria-hidden="true"></i>&nbsp;
+              <span>Clinic</span>
+            </div>
+            <div class="address">
+              <img src="./location-marker.svg" /> &nbsp;
+              <span>Ojo, Lagos 102101, Lagos</span>
+            </div>
+            <div class="phone">
+              <i class="fa fa-phone" aria-hidden="true"></i> &nbsp;
+              <span>N/A</span>
+            </div>
+            <div class="email">
+              <i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;
+              <span>N/A</span>
+            </div>
+          </div>
+        </div>
+        <div class="detail-card">
+          <img src="https://lh5.googleusercontent.com/p/AF1QipMui4lkCP9J2wHxPdwIN9dTOf_LCrlG7Zg52Wnb=w408-h544-k-no"
+            alt="" />
+          <div class="content">
+            <div class="name">
+              <i class="fa fa-hospital" aria-hidden="true"></i>&nbsp;
+              <h3>Marigold Hospital</h3>
+            </div>
+            <div class="type">
+              <img src="./hospital.svg" alt="" />&nbsp;
+              <span>Hospital</span>
+            </div>
+            <div class="address">
+              <img src="./location-marker.svg" /> &nbsp;
+              <span>12/14 Adeniyi Adefioye Street, Ikate, Lagos 101241, Lagos</span>
+            </div>
+            <div class="phone">
+              <i class="fa fa-phone" aria-hidden="true"></i> &nbsp;
+              <span>08033925500</span>
+            </div>
+            <div class="email">
+              <i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;
+              <span>N/A</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- <a href="#" class="cta-btn">Find a Hospital in your Region</a> -->
+
+      <div class="admin-card">
+        <div>
+          <h3>Become an Admin</h3>
+          <p>
+            Join the elite league of admins adding entries to our database,
+            and enlarging our provider network.
+          </p>
+        </div>
+        <a href="#" class="admin-cta-btn">Join Today</a>
+      </div>
+    </main>
+    <footer>
+      <div class="footer-logo">
+        <img src="/logo.svg" alt="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+          minus ab, suscipit praesentium quibusdam inventore at reprehenderit
+          quas officia pariatur!
+        </p>
+      </div>
+      <div class="socials">
+        <a href=""><img src="/facebook-icon.svg" alt="" /></a>
+        <a href="
+          #"><img src="/x-icon.svg" alt="" /></a>
+      </div>
+
+      <div class="useful-links">
+        <div class="nav">
+          <h4>Navigation</h4>
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div class="contribute">
+          <h4>Community</h4>
+          <ul>
+            <li><a href="#" class="btn">Login</a></li>
+            <li><a href="#" class="btn">Signup</a></li>
+            <li><a href="#">Contributors</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="copy">
+        <i class="fa fa-copyright" aria-hidden="true"></i> &nbsp;
+        <span>2024</span>
+        &nbsp;
+        <a href="https://github.com/dmystical-coder">dmystical-coder</a>
+      </div>
+    </footer>
+  </div>
 </template>
+
+<style scoped>
+.cta-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.75rem 1.5rem;
+  border-radius: 5.625rem;
+  border: 1px solid #2aa7ff;
+  color: #2aa7ff;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.5rem;
+}
+
+.admin-card {
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  border-radius: 2rem;
+  background: #2aa8ff;
+  text-align: center;
+  margin-block: 6rem;
+  padding: 7rem 2rem;
+}
+
+.admin-card>div {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.admin-card>div>h3 {
+  font-size: 1.5rem;
+  font-weight: 800;
+  line-height: 2.25rem;
+}
+
+.admin-card>div>p {
+  color: #fafbfe;
+  font-size: 1rem;
+  line-height: 1.5rem;
+}
+
+.admin-cta-btn {
+  color: #ffffff;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.5rem;
+  border-radius: 5.625rem;
+  border: 1px solid #ffffff;
+  align-self: center;
+  padding: 0.75rem 2rem;
+}
+</style>
